078d87d42ff010d05416d8515ab0b8bf3dd05352
nathalialialia <nathaliasgds@gmail.com>
2020-05-19 08:20:08 -0300

    Leitura volume

--- main.cpp
+++ main.cpp
@@ -2,12 +2,16 @@
 
 
 int main() {
+	int volume, ticket;
 
   cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
   cout << "--------------------------------" << endl;
   cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
   
-
+  cout << "Quantas compras o cliente fez no ultimo ano?"<<endl;
+  cin >> volume;
+  cout << "Qual o ticket medio?" << endl;
+  cin >> ticket;
 
 
 
