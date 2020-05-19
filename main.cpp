eee5b8bf2f2fc9d1198e718159e66fc2d8f1dae9
nathalialialia <nathaliasgds@gmail.com>
2020-05-19 08:12:28 -0300

    Cabecalho

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char** argv) {
+int main() {
+
+  cout << "SISTEMA DE PERFIL DE CLIENTE" << endl;
+  cout << "--------------------------------" << endl;
+  cout << "INFORME OS DADOS DO ULTIMO ANO" << endl;
+  
+
+
+
+
+
+
 	return 0;
 }
